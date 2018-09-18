259dc2d94f28ca46fddfe393b49ce1bfceb53b31
Samarpita Patra <s.patra@berkeley.edu>
2018-09-18 11:38:06 -0700

    changed alg

--- src/org/ucb/c5/composition/RBSChooser2.java
+++ src/org/ucb/c5/composition/RBSChooser2.java
@@ -1,9 +1,11 @@
 package org.ucb.c5.composition;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
 import org.ucb.c5.composition.model.RBSOption;
+import org.ucb.c5.sequtils.CalcEditDistance;
+import org.ucb.c5.sequtils.HairpinCounter;
+import org.ucb.c5.sequtils.Translate;
 import org.ucb.c5.utils.FileUtils;
 
 /**
@@ -12,19 +14,66 @@ import org.ucb.c5.utils.FileUtils;
  * Employs a list of genes and their associated ribosome binding sites for
  * highly-expressed proteins in E. coli.
  *
+ * @author Samarpita Patra samarpitap
  * @author J. Christopher Anderson
  */
 public class RBSChooser2 {
 
     private List<RBSOption> rbss;
-    //TODO:  Fill in
+    private HashMap<String, String[]> ecoliGenes;
+
 
     public void initiate() throws Exception {
+
+        //initialize HashMap that stores rbs name and its original cds
+        ecoliGenes = new HashMap<>();
+
+        //initialize rbss
+        rbss = new ArrayList<>();
+
+        //initialize Translate
+        Translate AminoAcids = new Translate();
+
+
         //Read the data file
         String data = FileUtils.readResourceFile("composition/data/coli_genes.txt");
 
-        //TODO:  Fill in
+        //Creating the HashMap for coli_genes.txt
+        String[] lines_data = data.split("\\r|\\r?\\n");
+        for (int i = 0; i < lines_data.length; i++) {
+            String line = lines_data[i];
+            String[] tabs = line.split("\t");
+
+            //value will have name of ecoli at i = 0
+            //cds at i = 6
+            ecoliGenes.put(tabs[1], tabs);
+
+        }
+
+        //Read rbs file
+        String rbsOptions = FileUtils.readResourceFile("composition/data/rbs_options.txt");
+
+        //Create RBS list
+        String[] lines_rbs = rbsOptions.split("\\r|\\r?\\n");
+        for (int i = 0; i <lines_rbs.length; i++) {
+            String line = lines_rbs[i];
+            String[] tabs = line.split("\t");
+
+            String rName = tabs[0];
+            String description = "from ecoli: " + ecoliGenes.get(rName)[0];
+            String rbs = tabs[1];
+            String cds = ecoliGenes.get(rName)[6];
+            String first6aas =  AminoAcids.run(cds.substring(0,17));
+
+            rbss.add(new RBSOption(rName, description, rbs, cds, first6aas));
+
+
+        }
+
+
     }
+
+
 
     /**
      * Provided a protein of sequence 'peptide', this computes the best ribosome
@@ -32,22 +81,47 @@ public class RBSChooser2 {
      * 
      * It also permits a list of options to exclude.
      *
-     * @param peptide The protein sequence, ie MSKGEE...
+     * @param cds The DNA sequence, ie ATCG
      * @param ignores The list of RBS's to exclude
      * @return
      * @throws Exception
      */
-    public RBSOption run(String peptide, Set<RBSOption> ignores) throws Exception {
-        //TODO:  Fill in
-        
-        return null;
+    public RBSOption run(String cds, Set<RBSOption> ignores) throws Exception {
+
+        //initialize distance calculator
+        CalcEditDistance calcdist = new CalcEditDistance();
+
+        RBSOption bestRBS = null;
+        int bestDist = 1000;
+        double bestHairpin = 100.00;
+
+        for (RBSOption option: rbss) {
+
+            if (ignores.contains(option)){
+                continue;
+            }
+
+            int dist = calcdist.run(cds, option.getCds());
+            double hairpinCount = new HairpinCounter().run(option.getRbs()+cds.substring(0,6));
+
+            if (dist < bestDist && hairpinCount < bestHairpin) {
+                bestDist = dist;
+                bestHairpin = hairpinCount;
+                bestRBS = option;
+            }
+
+        }
+
+
+        return bestRBS;
     }
 
 
     public static void main(String[] args) throws Exception {
         //Create an example
-//        String peptide = "MLSDTIDTKQQQQQLHVLFIDSYDSFTYNVVRLIEQQTDISPGVNAVHVTTVHSDTFQSMDQLLPLLPLFDAIVVGPGPGNPNNGAQDMGIISELFENANGKLDEVPILGICLGFQAMCLAQGADVSELNTIKHGQVYEMHLNDAARACGLFSGYPDTFKSTRYHSLHVNAEGIDTLLPLCTTEDENGILLMSAQTKNKPWFGVQYHPESCCSELGGLLVSNFLKLSFINNVKTGRWEKKKLNGEFSDILSRLDRTIDRDPIYKVKEKYPKGEDTTYVKQFEVSEDPKLTFEICNIIREEKFVMSSSVISENTGEWSIIALPNSASQVFTHYGAMKKTTVHYWQDSEISYTLLKKCLDGQDSDLPGSLEVIHEDKSQFWITLGKFMENKIIDNHREIPFIGGLVGILGYEIGQYIACGRCNDDENSLVPDAKLVFINNSIVINHKQGKLYCISLDNTFPVALEQSLRDSFVRKKNIKQSLSWPKYLPEEIDFIITMPDKLDYAKAFKKCQDYMHKGDSYEMCLTTQTKVVPSAVIEPWRIFQTLVQRNPAPFSSFFEFKDIIPRQDETPPVLCFLSTSPERFLKWDADTCELRPIKGTVKKGPQMNLAKATRILKTPKEFGENLMILDLIRNDLYELVPDVRVEEFMSVQEYATVYQLVSVVKAHGLTSASKKTRYSGIDVLKHSLPPGSMTGAPKKITVQLLQDKIESKLNKHVNGGARGVYSGVTGYWSVNSNGDWSVNIRCMYSYNGGTSWQLGAGGAITVLSTLDGELEEMYNKLESNLQIFM";
+////        String peptide = "MLSDTIDTKQQQQQLHVLFIDSYDSFTYNVVRLIEQQTDISPGVNAVHVTTVHSDTFQSMDQLLPLLPLFDAIVVGPGPGNPNNGAQDMGIISELFENANGKLDEVPILGICLGFQAMCLAQGADVSELNTIKHGQVYEMHLNDAARACGLFSGYPDTFKSTRYHSLHVNAEGIDTLLPLCTTEDENGILLMSAQTKNKPWFGVQYHPESCCSELGGLLVSNFLKLSFINNVKTGRWEKKKLNGEFSDILSRLDRTIDRDPIYKVKEKYPKGEDTTYVKQFEVSEDPKLTFEICNIIREEKFVMSSSVISENTGEWSIIALPNSASQVFTHYGAMKKTTVHYWQDSEISYTLLKKCLDGQDSDLPGSLEVIHEDKSQFWITLGKFMENKIIDNHREIPFIGGLVGILGYEIGQYIACGRCNDDENSLVPDAKLVFINNSIVINHKQGKLYCISLDNTFPVALEQSLRDSFVRKKNIKQSLSWPKYLPEEIDFIITMPDKLDYAKAFKKCQDYMHKGDSYEMCLTTQTKVVPSAVIEPWRIFQTLVQRNPAPFSSFFEFKDIIPRQDETPPVLCFLSTSPERFLKWDADTCELRPIKGTVKKGPQMNLAKATRILKTPKEFGENLMILDLIRNDLYELVPDVRVEEFMSVQEYATVYQLVSVVKAHGLTSASKKTRYSGIDVLKHSLPPGSMTGAPKKITVQLLQDKIESKLNKHVNGGARGVYSGVTGYWSVNSNGDWSVNIRCMYSYNGGTSWQLGAGGAITVLSTLDGELEEMYNKLESNLQIFM";
         String peptide = "MSQTVHFQGNPVTVANSIPQ";
+        String cds = "atgagccagaccgtgcattttcagggcaacccggtgaccgtggcgaacagcattccgcag".toUpperCase();
 
         //Initiate the chooser
         RBSChooser2 chooser = new RBSChooser2();
@@ -61,11 +135,11 @@ public class RBSChooser2 {
         ignores.add(selected1);
 
         //Choose again with an ignore added
-        RBSOption selected2 = chooser.run(peptide, ignores);
+        RBSOption selected2 = chooser.run(cds, ignores);
 
         //Print out the two options, which should be different
         System.out.println("Protein starts with:");
-        System.out.println(peptide.substring(0, 6));
+        System.out.println(cds.substring(0, 6));
         System.out.println();
         System.out.println("Selected1:\n");
         System.out.println(selected1.toString());
---
+++ tests/org/ucb/c5/composition/RBSChooser2Test.java
@@ -0,0 +1,26 @@
+package org.ucb.c5.composition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RBSChooser2Test {
+
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+    }
+
+    //check for hairpins in test examples
+    //come up with sequences that cause hairpins--> run
+    //throw in an example--> output: rbs --> check no hairpins
+
+    @Test
+    public void secondTest() {
+//        Assert.assertTrue(false);
+    }
+
+
+
+}
